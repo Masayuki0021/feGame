bc5b688edd9dc05208489b116a4ae0ebf0d90fab
Masayuki0021 <higu0021@gmail.com>
2023-03-07 18:29:36 +0900

    3/7完成 チェックお願いします。

--- FEGameVer2/src/feGameVer2/ChooseGameMode.java
+++ FEGameVer2/src/feGameVer2/ChooseGameMode.java
@@ -5,18 +5,24 @@ import feGameVer2.gameMode.GameMode2;
 import feGameVer2.gameMode.GameMode3;
 
 public class ChooseGameMode {
-	public static void chooseGameMode() throws InterruptedException {
+	public static void chooseGameMode()  {
 		int gameNum=SetGameModeNumber.setGameModeNumbere();
 		//各ゲームモードインスタンス取得、メソッド呼び出し
-		if(gameNum==1) {
-			GameMode1 G1=new GameMode1();
-			G1.playGameMode1();
-		}else if(gameNum==2) {
-			GameMode2 G2=new GameMode2();
-			G2.playGameMode2();
-		}else if(gameNum==3) {
-			GameMode3 G3=new GameMode3();
-			G3.playGameMode3();
+		try{
+			if(gameNum==1) {
+				GameMode1 G1=new GameMode1();
+				G1.playGameMode1();
+			}else if(gameNum==2) {
+				GameMode2 G2=new GameMode2();
+				G2.playGameMode2();
+			}else if(gameNum==3) {
+				GameMode3 G3=new GameMode3();
+				G3.playGameMode3();
+			}
+		}catch(InterruptedException e){
+			System.out.println("起こりえないエラーが発生しています。\n"
+					+ "スレッドで問題が起きているかもしれません");
+			//本日聞いた例外処理の部分です
 		}
 	}
 }
