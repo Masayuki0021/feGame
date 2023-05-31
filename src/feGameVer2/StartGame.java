935ff85b2c61c74a672e527aeac25a770d3b295d
Masayuki0021 <121288435+Masayuki0021@users.noreply.github.com>
2023-05-31 12:50:29 +0900

    Update StartGame.java

--- src/feGameVer2/StartGame.java
+++ src/feGameVer2/StartGame.java
@@ -13,15 +13,21 @@ public class StartGame {
 		CheckNumber checkNumber =new CheckNumber();
 		int gameNum=checkNumber.checkAndReturnNumber(1, 3);
 		//各ゲームモードインスタンス取得、メソッド呼び出し
-		if(gameNum==1) {
-			GameMode1 game1=new GameMode1();
-			game1.playGameMode1();
-		}else if(gameNum==2) {
-			GameMode2 game2=new GameMode2();
-			game2.playGameMode2();
-		}else if(gameNum==3) {
-			GameMode3 game3=new GameMode3();
-			game3.playGameMode3();
+		try {
+			if(gameNum==1) {
+				GameMode1 game1=new GameMode1();
+				game1.playGameMode1();
+			}else if(gameNum==2) {
+				GameMode2 game2=new GameMode2();
+				game2.playGameMode2();
+			}else if(gameNum==3) {
+				GameMode3 game3=new GameMode3();
+				game3.playGameMode3();
+			}
+		} catch (InterruptedException e) {
+			//到達しえないエラー sleepメソッドのInterruptedExceptionに対応するが
+			//javaの単独使用では発生することは有り得ない
+			e.printStackTrace();
 		}
 	}
 }
